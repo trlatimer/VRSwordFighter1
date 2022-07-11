3640a281e1c9e1176d03c0fb403d529a5aac1f28
Tyler Latimer <trlatimer95@gmail.com>
2022-07-10 22:17:25 -0700

    Created initial network player

--- Assets/Scripts/IK/VRRig.cs
+++ Assets/Scripts/IK/VRRig.cs
@@ -1,5 +1,7 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 [System.Serializable]
@@ -28,18 +30,35 @@ public class VRRig : MonoBehaviour
     public Transform headConstraint;
     public Vector3 headBodyOffset;
 
+    public PhotonView photonView;
+
     void Start()
     {
         headBodyOffset = transform.position - headConstraint.position;
+        photonView = GetComponentInParent<PhotonView>();
+
+        if (photonView == null || photonView != null && photonView.IsMine)
+        {
+            if (head.vrTarget == null || leftHand.vrTarget == null || rightHand.vrTarget == null)
+            {
+                XROrigin origin = FindObjectOfType<XROrigin>();
+                head.vrTarget = origin.transform.Find("Camera Offset/Main Camera");
+                leftHand.vrTarget = origin.transform.Find("Camera Offset/LeftHand Controller");
+                rightHand.vrTarget = origin.transform.Find("Camera Offset/RightHand Controller");
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        transform.position = headConstraint.position + headBodyOffset;
-        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
+        if (photonView == null || photonView != null && photonView.IsMine) // If null, we are in a lobby screen with no networked player
+        {
+            transform.position = headConstraint.position + headBodyOffset;
+            transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
 
-        head.Map();
-        leftHand.Map();
-        rightHand.Map();
+            head.Map();
+            leftHand.Map();
+            rightHand.Map();
+        }     
     }
 }
